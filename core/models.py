5acb1afdd055dbac4b9cfae0ab09f2af05eb43dc
Jonathansl1 <jonathanbrendo3@gmail.com>
2025-10-07 15:47:37 -0300

    Atualizando projeto

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Contato(models.Model):
     nome = models.CharField(max_length=20)
     sobrenome = models.CharField(max_length=10)
-    descricao=models.TextField(max_length=255)
-    telefone = models.IntegerField()
+    descricao = models.TextField(max_length=255)
+    telefone = models.CharField(max_length=20)  # Alterado de IntegerField para CharField
     imagem = models.ImageField(upload_to='core_image/%Y/%m', blank=True, null=True)
-
+    ########
